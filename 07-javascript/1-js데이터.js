c8c397ce6b2f8937b12d11962266a9bd6b85e21f
0nejean <onejean114@gmail.com>
2023-09-11 11:41:53 +0900

    javascript for a while

---
+++ 07-javascript/1-js데이터.js
@@ -0,0 +1,11 @@
+// string 문자열
+const string1 = "Hello";
+const string2 = "Hello";
+const string3 = `바보 ${string1} ${1 + 1}`;
+
+console.log(string1, string2, string3);
+
+// number 숫자
+const number = 123;
+
+console.log(number);
